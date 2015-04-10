05bc1fca32bb88c2c06d7bbbc0c4871a837acf60
Alexandrakis Alexandros <aalexandrakis@hotmail.com>
2015-04-10 16:09:00 +0300

    -- add env vars

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h2>Test e-shop</h2>";
 echo getenv('FRUITSHOP_URL');
-header('Location: '.getenv('FRUITSHOP_URL').'fruit-e-shop');
+header('Location: '.getenv('FRUITSHOP_URL').'/fruit-e-shop');
 ?>
